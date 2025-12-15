5e0655958e40e65dccf6f261ebfb24a260990829
christian allred <christian.allred@tavahealth.com>
2025-12-15 12:55:55 -0700

    updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,22 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	lazy = false,
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"javascript",
-				"typescript",
-				"lua",
-				"go",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  lazy = false,
+  config = function()
+    require("nvim-treesitter.config").setup({
+      ensure_installed = {
+        "javascript",
+        "typescript",
+        "lua",
+        "go",
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
 }
